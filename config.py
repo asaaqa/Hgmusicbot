06e1077fa12ca4237a1b41c3b1eed31c1019d445
Desha_Elfager <91533908+MostafaShalaby1@users.noreply.github.com>
2023-03-09 06:01:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/f0f1f6aef799e282ba0a1.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/f0f1f6aef799e282ba0a1.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/hindichat_house")
-SUPPORT_CHANNEL = getenv("SUPPORT_GROUP", "https://t.me/theromeo_network")
+SUPPORT_CHAT = getenv("قناة المبرمج", "https://t.me/W_II_Y")
+SUPPORT_CHANNEL = getenv("جروب الدعم", "https://t.me/+wxZC6M1nTBQyNmU8")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1420171187").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/f0f1f6aef799e282ba0a1.jpg"
