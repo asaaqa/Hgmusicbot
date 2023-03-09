f8e167922ae7b9fc2ba2d1a51e93cca92f5bf001
Desha_Elfager <91533908+MostafaShalaby1@users.noreply.github.com>
2023-03-09 06:08:00 +0200

    Update stop.py

--- FallenMusic/Modules/stop.py
+++ FallenMusic/Modules/stop.py
@@ -31,7 +31,6 @@ from FallenMusic.Helpers import _clear_, admin_check, close_key
 @admin_check
 async def stop_str(_, message: Message):
     try:
-        await message.delete()
     except:
         pass
     try:
